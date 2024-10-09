1b665b27695265b558fdba772016cc9ca7773128
Ammar Ahmed <ammare.ba720.com>
2024-10-10 01:07:24 +0300

    Finish radio screen

--- lib/tabs/radio/radio_tab.dart
+++ lib/tabs/radio/radio_tab.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:islamic_app/app_theme.dart';
 
 class RadioTab extends StatelessWidget {
   const RadioTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const Center(
-        child: Text('radio')
+     double screenHeight = MediaQuery.sizeOf(context).height;
+    double screenWidth = MediaQuery.sizeOf(context).width;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/images/radio_image.png'),
+        SizedBox(height: screenHeight * 0.08),
+        Text(
+          'إذاعة القرآن الكريم',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        SizedBox(height: screenHeight * 0.05),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.skip_previous,
+              color: AppTheme.lightPrimary,
+              size: 35,
+            ),
+            SizedBox(width: screenWidth * 0.1,),
+             const Icon(
+              Icons.play_arrow_rounded,
+              color: AppTheme.lightPrimary,
+              size: 70,
+            ),
+            SizedBox(width: screenWidth * 0.1,),
+             const Icon(
+              Icons.skip_next,
+              color: AppTheme.lightPrimary,
+              size: 35,
+            ),
+          ],
+        )
+      ],
     );
   }
 }
